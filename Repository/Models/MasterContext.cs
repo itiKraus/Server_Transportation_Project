2d5d22e252ab7c8f4d123e5d335d41e7886a7c16
iti kraus <iti0534103050@gmail.com>
2023-04-26 16:51:35 +0300

    buildProject

---
+++ Repository/Models/MasterContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Models;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TestEmployee> TestEmployees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-NHIB2FA6;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TestEmployee>(entity =>
+        {
+            entity.HasKey(e => e.EmpId).HasName("PK__test_emp__AF2DBB99B1AB9D9C");
+
+            entity.ToTable("test_employee");
+
+            entity.Property(e => e.EmpId).ValueGeneratedNever();
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Salary)
+                .HasDefaultValueSql("((6500))")
+                .HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
